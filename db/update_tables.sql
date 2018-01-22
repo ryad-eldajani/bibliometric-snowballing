98bb5ced97b1444e911cd8f1463b284a61da8b11
Ryad El-Dajani <ryad.eldajani@posteo.de>
2018-01-22 19:18:52 +0100

    Fix update_tables.sql to include dropping of foreign keys

--- db/update_tables.sql
+++ db/update_tables.sql
@@ -4,9 +4,11 @@
 ALTER TABLE `work` ADD `created_at` DATETIME DEFAULT NOW();
 
 DELETE FROM `quote`;
+ALTER TABLE `quote` DROP FOREIGN KEY `fk_quote_id_work_quoted`;
+ALTER TABLE `quote` DROP FOREIGN KEY `fk_quote_id_work`;
 ALTER TABLE `quote` DROP PRIMARY KEY;
-ALTER TABLE `quote` DROP COLUMN `id_work`;
-ALTER TABLE `quote` DROP COLUMN `id_work_quoted`;
 ALTER TABLE `quote` ADD `doi_work` VARCHAR(255) NOT NULL;
 ALTER TABLE `quote` ADD `doi_work_quoted` VARCHAR(255) NOT NULL;
+ALTER TABLE `quote` DROP COLUMN `id_work`;
+ALTER TABLE `quote` DROP COLUMN `id_work_quoted`;
 ALTER TABLE `quote` ADD PRIMARY KEY (`doi_work`, `doi_work_quoted`);
